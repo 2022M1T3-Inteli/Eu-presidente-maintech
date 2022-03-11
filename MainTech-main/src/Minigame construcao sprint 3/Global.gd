c56f2d80012fd4509d36d708ad1fe33ca0347f02
lenagallas <helena.gallas@sou.inteli.edu.br>
2022-03-11 08:16:12 -0300

    mini game construção

---
+++ MainTech-main/src/Minigame construcao sprint 3/Global.gd
@@ -0,0 +1,20 @@
+extends Node
+var baixo = false
+var botao = false
+var conta = 1
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text
+func ready():	
+	pass
+ # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
