bb41ed53aeb1c09a8585674983a1390f7921b400
SarahInteli <sarah.ribeiro@sou.inteli.edu.br>
2022-03-24 18:11:00 -0300

    intro da emenda constitucional

---
+++ MainTech-main/src/Atualizacao_24-03 -sarah/code/global.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var velocidade = 6
+var pontuacao = 0
+var gameover = false
+var gP = 0
+var gN = 0
+var skinsGames = ""
+var fase = ""
+var personagem = 0
+var nomePessoa = ""
+var apCongreco = 80
+var apPopulacao = 60
+
+
+func expandir (obj):
+	obj.scale = Vector2(0.25,0.25)
+	for i in range (0,5):
+		obj.scale = Vector2(0.25*i,0.25*i)
+		i += 1
+		yield(get_tree().create_timer(0,25),"timeout")
---
+++ MainTech-main/src/Atualizacao_24-03 -sarah/code/obstaculo.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var rng = RandomNumberGenerator.new()
+var posicaoObstaculo = 0
+
+func _ready():
+	rng.randomize()
+	posicaoObstaculo = rng.randi_range(0, 3) # gera um número aleatório (0, 1 ou 2)
+	if posicaoObstaculo == 0:
+		$".".position.x = 246 # caso a posicaoColetavel for 0, a posição x do coletável será 307
+	elif posicaoObstaculo == 1:
+		$".".position.x = 512 # caso a posicaoColetavel for 1, a posição x do coletável será 512
+	else:
+		$".".position.x = 780 # caso a posicaoColetavel for 2, a posição x do coletável será 716
+
+func _process(delta):
+	$".".position.y += Global.velocidade
+	if $".".position.y > 600:
+		queue_free()
+
+func _on_obstaculo_body_entered(body):
+	if body.name == "player":
+		Global.gameover = true
